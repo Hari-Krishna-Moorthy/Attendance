d067ceac7d4b93ce3325482bfca036b864a13a4f
Hari-Krishna-Moorthy <harikrishnamoorthyt@yahoo.com>
2022-02-27 21:43:36 +0530

    Adding Files

--- Attendance/settings.py
+++ Attendance/settings.py
@@ -123,7 +123,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
@@ -155,7 +161,7 @@ EMAIL_HOST_PASSWORD =  'Project@2022'
 EMAIL_PORT = 587
 
 
-# import django_heroku
-# django_heroku.settings(locals())
+import django_heroku
+django_heroku.settings(locals())
 
 
